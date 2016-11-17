09dbf3da45e62d5a0e0f65bece499b44944df5f5
André Lunelli <to.lunelli@gmail.com>
2016-11-17 19:47:45 -0200

    adicionado txt com a representacao da adjacencia dos grafos

--- src/main/java/br/furb/grafos/caminhamento_grafo/Grafo.java
+++ src/main/java/br/furb/grafos/caminhamento_grafo/Grafo.java
@@ -7,24 +7,25 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-public class Grafo {
+public class Grafo {		
 	
-	private static String rotulos[] = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-	
+	private int[][] matrizAdjacencia;
+
+	public Grafo(int[][] matrizAdjacencia) {
+		this.matrizAdjacencia = matrizAdjacencia;
+	}
+
 	public static void main(String[] args) {
 	try (BufferedReader br = new BufferedReader(new FileReader("grafo.txt"))) {
 			int nVertices = 0;
 			int matrizAdjacencia[][] = null;
-			String rotulo[] = null;
 			String line;
 			int nRotulo = 0;			
 			while ((line = br.readLine()) != null) {
 				if (nVertices == 0) {
 					nVertices = Integer.parseInt(line);
 					matrizAdjacencia = new int[nVertices][nVertices];
-					rotulo = new String[nVertices];
 				} else {
-					rotulo[nRotulo] = rotulos[nRotulo];
 					String[] split = line.split(",");
 					int j = 0;
 					for (String string : split) {
@@ -35,10 +36,17 @@ public class Grafo {
 					nRotulo++;
 				}
 			}
+			Grafo grafo = new Grafo(matrizAdjacencia);
+			grafo.eulerizar();
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	private void eulerizar() {
+		// TODO Auto-generated method stub
+		
 	}
 }
